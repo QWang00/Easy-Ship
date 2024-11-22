90b6cd3f86e46e0bad81ba6eb69ce03d1d4b7da9
qiuqiu <qiujuanwang87@gmail.com>
2024-11-22 11:35:28 +0000

    feat: add ItemNotFoundException for handling not found errors

---
+++ src/main/java/com/easysupplychain/exception/ItemNotFoundException.java
@@ -0,0 +1,11 @@
+package com.easysupplychain.exception;
+
+public class ItemNotFoundException extends RuntimeException {
+
+    public ItemNotFoundException() {
+    }
+
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+}
